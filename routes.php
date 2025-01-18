36eb5ec32de05444882e99942a3e186dd36fbc78
KingJoshua <kingjayeriughe@gmail.com>
2025-01-18 15:25:34 +0100

    FEAT: add get route to submit bill

--- routes.php
+++ routes.php
@@ -7,3 +7,6 @@ if ($_SERVER['SERVER_NAME'] === 'localhost') {
 
 $router->get("{$baseURI[1]}/", 'HomeController@index');
 $router->get("{$baseURI[1]}/create-bill", 'HomeController@create_bill');
+
+
+$router->post("{$baseURI[1]}/submit-bill", 'HomeController@submit_bill');
