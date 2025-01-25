60a91ebd4213cb2722e6c1a4a0f2bc06e47bcbec
KingJoshua <kingjayeriughe@gmail.com>
2025-01-25 09:41:14 +0100

    FEAT: add get route to load single guest bill

--- routes.php
+++ routes.php
@@ -8,6 +8,7 @@ if ($_SERVER['SERVER_NAME'] === 'localhost') {
 $router->get("{$baseURI[1]}/", 'HomeController@index');
 $router->get("{$baseURI[1]}/create-bill", 'HomeController@create_bill');
 $router->get("{$baseURI[1]}/view-bills", 'HomeController@view_bills');
+$router->get("{$baseURI[1]}/view/guest-bill/{id}", 'HomeController@view_guest_bill');
 
 
 $router->post("{$baseURI[1]}/submit-bill", 'HomeController@submit_bill');
