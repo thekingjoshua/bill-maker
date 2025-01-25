05d69067aeb66bf50d9df602e4023cc99096996d
KingJoshua <kingjayeriughe@gmail.com>
2025-01-25 11:16:33 +0100

    FEAT: add get route to load add-organization view

--- routes.php
+++ routes.php
@@ -9,6 +9,7 @@ $router->get("{$baseURI[1]}/", 'HomeController@index');
 $router->get("{$baseURI[1]}/create-bill", 'HomeController@create_bill');
 $router->get("{$baseURI[1]}/view-bills", 'HomeController@view_bills');
 $router->get("{$baseURI[1]}/view/guest-bill/{id}", 'HomeController@view_guest_bill');
+$router->get("{$baseURI[1]}/add/organization", 'HomeController@add_organization');
 
 
 $router->post("{$baseURI[1]}/submit-bill", 'HomeController@submit_bill');
